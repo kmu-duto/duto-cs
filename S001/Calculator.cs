52a3e5b001402d945bacc6c29b3ca7d97e43bcd2
deveb1479@gmail.com <deveb1479@gmail.com>
2023-05-12 14:26:09 +0900

    s001 in 0512

---
+++ S001/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S001
+{
+    internal class Calculator
+    {
+        private int a, b;
+
+        public Calculator(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int Add() { return a + b; }
+        public int Sub() { return a - b; }
+        public int Mul() { return a * b; }
+        public int Div() { return a / b; }
+    }
+}
