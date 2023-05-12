42d9a9ca77d25d49d1295143367de6cb5fd6cf37
deveb1479@gmail.com <deveb1479@gmail.com>
2023-05-12 13:00:16 +0900

    start

---
+++ S001/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace S001
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");   
+        }
+    }
+}
